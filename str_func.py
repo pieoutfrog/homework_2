279b6ee574387ced19e0ddcf7784607da7f0b57e
pieoutfrog <127651106+pieoutfrog@users.noreply.github.com>
2023-03-16 02:35:54 +1000

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,11 +1,5 @@
 def upper_input():
-    """WOW"""
+    """Let`s imagine I`ve done something"""
     user_input = input()
     upper_input = user_input.upper()
     return upper_input
-
-def up_main():
-    """Something`s happening"""
-    user_input = input()
-    up_main_let = user_input.title()
-    return up_main_let
