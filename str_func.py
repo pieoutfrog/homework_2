1c035a2efa9bf045721f753fbb76d3dc31156dcc
pieoutfrog <127651106+pieoutfrog@users.noreply.github.com>
2023-03-16 02:10:07 +1000

    Update str_func.py
    heh

--- str_func.py
+++ str_func.py
@@ -1,5 +1,5 @@
 def upper_input():
-    """Let`s imagine I did something"""
+    """Let`s imagine I`ve done something"""
     user_input = input()
     upper_input = user_input.upper()
     return upper_input
