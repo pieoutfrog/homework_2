b2aa1c90aee00a05f23b17a6a10f8dee4a7f8efa
Alice <despero45@gmail.com>
2023-03-16 02:16:45 +1000

    doctring for everybody

--- str_func.py
+++ str_func.py
@@ -1,4 +1,11 @@
 def upper_input():
+    """I love dota so much"""
     user_input = input()
     upper_input = user_input.upper()
     return upper_input
+
+def up_main():
+    """Something`s happening"""
+    user_input = input()
+    up_main_let = user_input.title()
+    return up_main_let
